7b9d5eca632ff52d68899296df828ab2921a6f64
zengyannan <as61912862@163.com>
2018-04-13 17:36:18 +0800

    create demo

---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+    <h1>site : {{site}}</h1>
+    <h1>url : {{url}}</h1>
+</div>
+</template>
+<script type="text/javascript">
+    /* eslint-disable */
+    export default {
+        name: 'test',
+        data () {
+            return {
+             site: '1111',
+             url: 'www.baidu.com'
+            }
+        }
+    }
+    /* eslint-disable */
+</script>
